9043d3366bb72a5f1d2ec004ff7b8c68c476e84d
John Berthels <john.berthels@gmail.com>
2023-12-04 07:56:31 +0000

    day4 part2

--- y2023_day4.ts
+++ y2023_day4.ts
@@ -4,8 +4,8 @@ import * as P from "./pts";
 export const run = (a: aoc.Aoc): void => {
     const p1 = calcp1(a);
     console.log(`Part 1: ${p1}`);
-    //    const p2 = calcp2(a);
-    //    console.log(`Part 2: ${p2}`);
+    const p2 = calcp2(a);
+    console.log(`Part 2: ${p2}`);
 };
 
 interface card {
@@ -15,12 +15,16 @@ interface card {
 }
 
 const cardScore = (c: card): number => {
-    const isWinner = (n: number) => c.winning.includes(n);
-    const winners = c.has.filter(isWinner);
-    if (winners.length == 0) {
+    const numWinners = cardNumWinners(c);
+    if (numWinners == 0) {
         return 0;
     }
-    return Math.pow(2, winners.length - 1);
+    return Math.pow(2, numWinners - 1);
+};
+
+const cardNumWinners = (c: card): number => {
+    const isWinner = (n: number) => c.winning.includes(n);
+    return c.has.filter(isWinner).length;
 };
 
 const parseLine = (l: string): card => {
@@ -37,13 +41,26 @@ const parseLine = (l: string): card => {
         .filter((s) => s.length > 0)
         .map(aoc.strToNum);
     const result = { id: Number(m[1]), winning: wins, has: hass };
-    console.log(result);
+    //console.log(result);
     return result;
 };
 
 const calcp1 = (a) => {
     const cards = a.lines.map(parseLine);
     const scores = cards.map(cardScore);
-    console.log(scores);
+    //    console.log(scores);
     return aoc.sum(scores);
+};
+
+const calcp2 = (a) => {
+    const cards = a.lines.map(parseLine);
+    const numWinners = cards.map(cardNumWinners);
+    let cardCounts = new Array<number>(numWinners.length).fill(1);
+    numWinners.forEach((n, i) => {
+        for (let j = 1; j <= n; j++) {
+            cardCounts[i + j] += cardCounts[i];
+        }
+    });
+    console.log(cardCounts);
+    return aoc.sum(cardCounts);
 };
